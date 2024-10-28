997d6ae3f3dd89def65dfb3470ed306bb85583ba
MarMakHH <bhi021@myy.haaga-helia.fi>
2024-10-28 13:16:42 +0200

    added entity repos

---
+++ src/main/java/s24/backend/domain/ProductRepository.java
@@ -0,0 +1,7 @@
+package s24.backend.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+
+}
---
+++ src/main/java/s24/backend/domain/TypeRepository.java
@@ -0,0 +1,7 @@
+package s24.backend.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TypeRepository extends CrudRepository<Type, Long> {
+
+}
