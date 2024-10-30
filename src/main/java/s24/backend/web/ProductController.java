520c984e042fed27ea555e3f9bf5a1f044b786db
Jonna Rinne <jonna.rinne1@gmail.com>
2024-10-30 20:25:06 +0200

    Delete product

--- src/main/java/s24/backend/web/ProductController.java
+++ src/main/java/s24/backend/web/ProductController.java
@@ -1,5 +1,26 @@
 package s24.backend.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import s24.backend.domain.Product;
+import s24.backend.domain.ProductRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
 public class ProductController {
+
+
+    // Poistetaan tuote
+    @GetMapping("delete/{id}")
+	public String deleteProduct(@PathVariable("id") Long id, Model model) {
+		log.info("delete product " + id);
+		ProductRepository.deleteById(id);
+		return "redirect:/";
+	}
+    
 
 }
