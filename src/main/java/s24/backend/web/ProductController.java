86e26b7e211ce041b67cc4e221f9f7d8f45bb463
bhi021@myy.haaga-helia.fi <bhi021@myy.haaga-helia.fi>
2024-10-31 16:13:11 +0200

    Fixed addProduct

--- src/main/java/s24/backend/web/ProductController.java
+++ src/main/java/s24/backend/web/ProductController.java
@@ -5,8 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import s24.backend.domain.ManufacturerRepository;
 import s24.backend.domain.Product;
 import s24.backend.domain.ProductRepository;
+import s24.backend.domain.SizeRepository;
+import s24.backend.domain.TypeRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -19,6 +22,15 @@ public class ProductController {
     @Autowired
     private ProductRepository productrepo;
 
+    @Autowired
+    private SizeRepository sizerepo;
+
+    @Autowired
+    private TypeRepository typerepo;
+
+    @Autowired
+    private ManufacturerRepository manufacturerrepo;
+
     @RequestMapping(value = "/productList", method = RequestMethod.GET)
     public String showProducts(Model model) {
         model.addAttribute("products", productrepo.findAll());
@@ -28,6 +40,9 @@ public class ProductController {
     @RequestMapping(value = "/addProduct", method = RequestMethod.GET)
     public String addProduct(Model model) {
         model.addAttribute("product", new Product());
+        model.addAttribute("sizes", sizerepo.findAll());
+        model.addAttribute("types", typerepo.findAll());
+        model.addAttribute("manufacturers", manufacturerrepo.findAll());
         return "addProduct";
     }
 
