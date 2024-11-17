83f5114ae2f246c19ef8a9800180a9628333a8eb
bhi021@myy.haaga-helia.fi <bhi021@myy.haaga-helia.fi>
2024-11-17 18:30:25 +0200

    added cors for products

--- src/main/java/s24/backend/web/RestProductController.java
+++ src/main/java/s24/backend/web/RestProductController.java
@@ -3,6 +3,7 @@ package s24.backend.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +22,7 @@ public class RestProductController {
     private ProductRepository productrepo;
 
     //Get all products
+    @CrossOrigin
     @GetMapping("/products")
     public Iterable<Product> getProducts() {
         return productrepo.findAll();
