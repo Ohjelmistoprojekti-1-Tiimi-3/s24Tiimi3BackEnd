543c8517abf1e52560acf399d00657fa50befd3b
bhi021@myy.haaga-helia.fi <bhi021@myy.haaga-helia.fi>
2024-11-28 12:20:58 +0200

    Added add new order functionality

--- src/main/java/s24/backend/web/OrderController.java
+++ src/main/java/s24/backend/web/OrderController.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import s24.backend.domain.Customer;
 import s24.backend.domain.CustomerRepository;
+import s24.backend.domain.Order;
 import s24.backend.domain.OrderRepository;
+import s24.backend.domain.ProductRepository;
 
 @Controller
 public class OrderController {
@@ -19,9 +23,12 @@ public class OrderController {
     @Autowired
     private CustomerRepository customerrepo;
 
+    @Autowired
+    private ProductRepository productrepo;
+
     @GetMapping("/orders")
     public String showOrders(Model model) {
-        model.addAttribute("orders", orderrepo.findAll());
+        model.addAttribute("customers", customerrepo.findAll());
         return "orders";
     }
 
@@ -30,5 +37,20 @@ public class OrderController {
         Customer c = customerrepo.findById(id).get();
         model.addAttribute("orders", orderrepo.findByCustomer(c));
         return "customerorder";
+    }
+
+    @GetMapping("/addorder")
+    public String addOrder(Model model) {
+        model.addAttribute("order", new Order());
+        model.addAttribute("products", productrepo.findAll());
+        model.addAttribute("customers", customerrepo.findAll());
+        return "addorder";
+    }
+
+    @PostMapping("/saveorder")
+    public String saveOrder(@ModelAttribute("order") Order order) {
+        order.setStatus("Tilaus käsittelyssä");
+        orderrepo.save(order);
+        return "redirect:orders";
     }
 }
