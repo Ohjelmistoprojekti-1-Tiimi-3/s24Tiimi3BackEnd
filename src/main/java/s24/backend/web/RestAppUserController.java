7195b919f7cecd54eabc3a01cdaad3f9d7e760e3
bhi021@myy.haaga-helia.fi <bhi021@myy.haaga-helia.fi>
2024-12-08 21:15:43 +0200

    add user rest password encoding

--- src/main/java/s24/backend/web/RestAppUserController.java
+++ src/main/java/s24/backend/web/RestAppUserController.java
@@ -3,6 +3,7 @@ package s24.backend.web;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,7 +33,9 @@ public class RestAppUserController {
     // Adding a new AppUser
     @PostMapping("/addappuser")
     public AppUser newAppUser(@RequestBody AppUser newAppUser) {
-
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        newAppUser.setPasswordhash(encoder.encode(newAppUser.getPasswordhash()));
+        newAppUser.setRole("ROLE_USER");
         return appuserrepo.save(newAppUser);
     }
 
